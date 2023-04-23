8dc847cc26c29f5004a0fbe98074c8186fe93368
Asher Gomez <ashersaupingomez@gmail.com>
2023-04-23 17:48:09 +1000

    initial commit

---
+++ bench.ts
@@ -0,0 +1,7 @@
+Deno.bench("Date.now()", { group: "timing", baseline: true }, () => {
+  Date.now();
+});
+
+Deno.bench("performance.now()", { group: "timing" }, () => {
+  performance.now();
+});
--- mod.ts
+++ mod.ts
@@ -1,3 +1,39 @@
-import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
+import { serve } from "std/http/server.ts";
+import { renderChart } from "fresh_charts/render.ts";
+import { ChartColors } from "fresh_charts/utils.ts";
+import bench from "./bench.json" assert { type: "json" };
 
-serve((_req) => new Response("Hello, World!"));
+async function main() {
+  await serve((_req) =>
+    renderChart({
+      data: {
+        labels: bench.benches.map(({ name }) => name),
+        datasets: [{
+          label: "Iterations",
+          data: bench.benches.map(({ results }) => results[0].ok.avg),
+          backgroundColor: ChartColors.Green,
+        }],
+      },
+      options: {
+        scales: {
+          y: {
+            title: {
+              display: true,
+              text: "Iterations",
+            },
+          },
+        },
+        plugins: {
+          title: {
+            display: true,
+            text: `${bench.cpu} ${bench.runtime}`,
+          },
+        },
+      },
+    })
+  );
+}
+
+if (import.meta.main) {
+  await main();
+}
