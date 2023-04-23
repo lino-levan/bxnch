ef3cbb03c6e6e6a11027882d0b8c1cd5df49b066
Asher Gomez <ashersaupingomez@gmail.com>
2023-04-23 18:28:38 +1000

    intial version

--- mod.ts
+++ mod.ts
@@ -1,37 +1,63 @@
+// deno-lint-ignore-file no-explicit-any
 import { serve } from "std/http/server.ts";
 import { renderChart } from "fresh_charts/render.ts";
 import { ChartColors } from "fresh_charts/utils.ts";
-import bench from "./bench.json" assert { type: "json" };
 
-async function main() {
-  await serve((_req) =>
-    renderChart({
-      data: {
-        labels: bench.benches.map(({ name }) => name),
-        datasets: [{
-          label: "Iterations",
-          data: bench.benches.map(({ results }) => results[0].ok.avg),
-          backgroundColor: ChartColors.Green,
-        }],
-      },
-      options: {
-        scales: {
-          y: {
-            title: {
-              display: true,
-              text: "Iterations",
-            },
-          },
-        },
-        plugins: {
+export function toRawFileUrl(url: URL) {
+  return url.toString().replace(
+    url.origin,
+    "https://raw.githubusercontent.com",
+  );
+}
+
+export async function handler(request: Request) {
+  if (request.method !== "GET") {
+    await request.body?.cancel();
+    return new Response(null, { status: 404 });
+  }
+
+  const url = new URL(request.url);
+  if (url.pathname === "/") {
+    return Response.redirect("https://github.com/iuioiua/bnch");
+  }
+
+  if (url.pathname === "/favicon.ico") {
+    return new Response(null, { status: 404 });
+  }
+
+  const response = await fetch(toRawFileUrl(url));
+  const bench = await response.json();
+
+  return await renderChart({
+    data: {
+      labels: bench.benches.map(({ name }: any) => name),
+      datasets: [{
+        label: "Iterations",
+        data: bench.benches.map(({ results }: any) => results[0].ok.avg),
+        backgroundColor: ChartColors.Green,
+      }],
+    },
+    options: {
+      scales: {
+        y: {
           title: {
             display: true,
-            text: `${bench.cpu} ${bench.runtime}`,
+            text: "Iterations",
           },
         },
       },
-    })
-  );
+      plugins: {
+        title: {
+          display: true,
+          text: `${bench.cpu} ${bench.runtime}`,
+        },
+      },
+    },
+  });
+}
+
+async function main() {
+  await serve(handler);
 }
 
 if (import.meta.main) {
